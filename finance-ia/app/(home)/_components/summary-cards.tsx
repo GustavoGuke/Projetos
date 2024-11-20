0c7261dee4f968da980604e115b6b0f1aba0777f
gusta <gustavo.athenas13@gmail.com>
2024-11-20 09:15:19 -0300

    feat: add search finance of month

--- finance-ia/app/(home)/_components/summary-cards.tsx
+++ finance-ia/app/(home)/_components/summary-cards.tsx
@@ -2,33 +2,68 @@ import { Card, CardContent, CardHeader } from '@/app/_components/ui/card'
 import { PiggyBankIcon, TrendingUpDownIcon, TrendingUpIcon, WalletIcon } from 'lucide-react'
 import React from 'react'
 import ResumeCard from './resume-card'
+import { db } from '@/app/_lib/prisma'
 
-export default function SummaryCards() {
+
+interface SummaryCardsProps {
+    month: string
+}
+
+export default async function SummaryCards({ month }: SummaryCardsProps) {
+    const where = {
+        date: {
+            gte: new Date(`${month}-01`),
+            lte: new Date(`${month}-30`),
+        },
+    }
+
+
+    const depositsTotal = Number(
+        (await db.transaction.aggregate({
+            where: { ...where, type: 'DEPOSIT' },
+            _sum: { amount: true }
+        })
+        )?._sum?.amount
+    )
+
+    const investimentsTotal = Number(
+        (await db.transaction.aggregate({
+            where: { ...where, type: 'INVESTMENT' },
+            _sum: { amount: true }
+        }))?._sum.amount
+    )
+    const expensesTotal = Number(
+        (await db.transaction.aggregate({
+            where: { ...where, type: 'EXPENSE' },
+            _sum: { amount: true }
+        }))?._sum.amount
+    )
+    const balance = depositsTotal - investimentsTotal - expensesTotal;
     return (
         <div className='space-y-6'>
             <ResumeCard
                 icon={<WalletIcon size={16} />}
                 title="Saldo"
-                amount={1000} 
+                amount={balance}
                 size='lg'
             />
-          
+
             <div className='grid grid-cols-3 gap-6'>
                 <ResumeCard
                     icon={<PiggyBankIcon size={16} />}
                     title="Investido"
-                    amount={1000} 
-                    />
+                    amount={investimentsTotal}
+                />
                 <ResumeCard
-                    icon={<TrendingUpIcon size={16} className='text-primary'/>}
+                    icon={<TrendingUpIcon size={16} className='text-primary' />}
                     title="Receita"
-                    amount={1000} 
-                    />
+                    amount={depositsTotal}
+                />
                 <ResumeCard
-                    icon={<TrendingUpDownIcon size={16} className='text-red-400'/>}
+                    icon={<TrendingUpDownIcon size={16} className='text-red-400' />}
                     title="Despesas"
-                    amount={1000} 
-                    />
+                    amount={expensesTotal}
+                />
             </div>
         </div>
     )
--- finance-ia/app/(home)/page.tsx
+++ finance-ia/app/(home)/page.tsx
@@ -4,16 +4,33 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { Navbar } from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
 
-export default async function Home() {
+
+interface HomeProps {
+  searchParams: { month: string };
+}
+export default async function Home({searchParams: {month}}: HomeProps) {
   const { userId } = await auth()
   if (!userId) {
     redirect("/login")
   }
+
+  const monthIsInvalid = !month || !isMatch(month, "MM")
+  const monthNumber= new Date().getMonth() 
+  if (monthIsInvalid) { 
+    redirect("?month=01")
+  }
+
   return (
     <>
       <Navbar />
-      <SummaryCards />
+      <div className="flex p-6 justify-between items-center">
+        <h1>Dashboard</h1>
+        <TimeSelect />
+      </div>
+      <SummaryCards month={month}/>
     </>
   );
 }
